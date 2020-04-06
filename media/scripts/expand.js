81cd1b09d1d8450df37bbca68cf2125745051564
xiecj <953560166@qq.com>
2020-04-06 23:25:08 +0800

    update from gridea: 2020-04-06 23:25:08

--- media/scripts/expand.js
+++ media/scripts/expand.js
@@ -1,9 +1,15 @@
-// 给markdown生成的超链接设置blank属性
 var aTags = Array.prototype.slice.apply(document.getElementsByTagName("a"));
 aTags.forEach(function (el) {
+    // 给链接带有'?_blank'参数的a标签设置_blank属性
     if (el.href.indexOf("?_blank") > -1) {
         el.target = "_blank";
         el.href = el.href.replace("?_blank", "");
+    }
+    // 点击当前窗口打开页面的链接时显示Loading
+    if(el.target == '_self' || el.target == '') {
+        el.addEventListener("click", function(){
+            document.querySelector('#loading').style = 'display: block;'
+        });
     }
 });
 
@@ -12,14 +18,5 @@ NodeList.prototype.forEach = NodeList.prototype.forEach || Array.prototype.forEa
 document.querySelectorAll('pre code').forEach(function (item) {
     if (item.textContent.trim().split('\n').length >= 10) {
         item.parentNode.className += ' line-numbers'
-    }
-})
-
-// 内部链接点击时显示loading
-document.querySelectorAll('a').forEach(function(el){
-    if(el.target == '_self' || el.target == '') {
-        el.addEventListener("click", function(){
-            document.querySelector('#loading').style = 'display: flex;'
-        });
     }
 })
