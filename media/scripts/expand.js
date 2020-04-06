c229b0460bef1340dbae6a9b199a603a55ab795e
xiecj <953560166@qq.com>
2020-04-06 21:46:38 +0800

    update from gridea: 2020-04-06 21:46:38

--- media/scripts/expand.js
+++ media/scripts/expand.js
@@ -9,19 +9,8 @@ aTags.forEach(function (el) {
 
 // 超过10行的代码块添加行号
 NodeList.prototype.forEach = NodeList.prototype.forEach || Array.prototype.forEach; // 某浏览器NodeList不支持forrEach方法
-document.querySelectorAll('pre code').forEach((item) => {
+document.querySelectorAll('pre code').forEach(function (item) {
     if (item.textContent.trim().split('\n').length >= 10) {
         item.parentNode.className += ' line-numbers'
     }
 })
-
-// 代码高亮插件
-if (document.querySelector('pre code')) {
-    document.write(`
-          <link rel="stylesheet" href="https://cdn.staticfile.org/highlight.js/9.18.1/styles/vs2015.min.css">
-          <script language=javascript src='https://cdn.staticfile.org/highlight.js/9.18.1/highlight.min.js'><\/script>
-          <script>hljs.initHighlightingOnLoad();<\/script>
-    
-          <script src="/media/scripts/prism.js"><\/script>
-        `);
-}
